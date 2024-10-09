26705e113d96c4aa0462788fba24370e1d5917a7
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 08:52:18 +0700

    Add ApplicationConfig 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootretrysample/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.springbootretrysample.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-retry-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/10/24
+ * Time: 08.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
