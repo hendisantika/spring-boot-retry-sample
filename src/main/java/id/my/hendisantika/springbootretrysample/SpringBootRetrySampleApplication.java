628953223c4fb1db4f88adebe7926fe74cc7df0d
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 08:48:28 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootretrysample/SpringBootRetrySampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootretrysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRetrySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRetrySampleApplication.class, args);
+    }
+
+}
