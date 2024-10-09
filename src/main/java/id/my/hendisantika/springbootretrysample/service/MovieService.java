fa6eb188ac926994c7a0d20942fafd6c1627c4a4
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 08:53:23 +0700

    Add getMovieDetails in MovieService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootretrysample/service/MovieService.java
+++ src/main/java/id/my/hendisantika/springbootretrysample/service/MovieService.java
@@ -1,8 +1,14 @@
 package id.my.hendisantika.springbootretrysample.service;
 
+import id.my.hendisantika.springbootretrysample.entity.Movie;
 import id.my.hendisantika.springbootretrysample.rest.client.MovieApiClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +25,25 @@ import org.springframework.stereotype.Service;
 public class MovieService {
 
     private final MovieApiClient movieApiClient;
+
+    @Retryable(
+            retryFor = {HttpServerErrorException.class, HttpClientErrorException.class, ResourceAccessException.class},
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 5000))
+    public Movie getMovieDetails(String movieId) throws ResourceAccessException {
+        Movie movie = null;
+        try {
+            movie = movieApiClient.getMovieDetails(movieId);
+        } catch (HttpServerErrorException httpServerErrorException) {
+            System.out.println("Received HTTP server error exception while fetching the movie details. Error Message: " + httpServerErrorException.getMessage());
+            throw httpServerErrorException;
+        } catch (HttpClientErrorException httpClientErrorException) {
+            System.out.println("Received HTTP client error exception while fetching the movie details. Error Message: " + httpClientErrorException.getMessage());
+            throw httpClientErrorException;
+        } catch (ResourceAccessException resourceAccessException) {
+            System.out.println("Received Resource Access exception while fetching the movie details.");
+            throw resourceAccessException;
+        }
+        return movie;
+    }
 }
